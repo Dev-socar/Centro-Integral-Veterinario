19dd69dab68dcd1d01a983f601046eb9dcdbc79c
Dev-socar <omar.fsc19@gmail.com>
2024-11-09 07:05:43 -0600

    Fixed

---
+++ src/interfaces/galeria/galeria.ts
@@ -0,0 +1,36 @@
+export interface Imagen {
+    src:string
+}
+
+export const imagenes: Imagen[] = [
+  {
+    src: "galeria-01.jpg",
+  },
+  {
+    src: "galeria-02.jpg",
+  },
+  {
+    src: "galeria-03.jpg",
+  },
+  {
+    src: "galeria-04.jpg",
+  },
+  {
+    src: "galeria-05.jpg",
+  },
+  {
+    src: "galeria-06.jpg",
+  },
+  {
+    src: "galeria-07.jpg",
+  },
+  {
+    src: "galeria-08.jpg",
+  },
+  {
+    src: "galeria-09.jpg",
+  },
+  {
+    src: "galeria-10.jpg",
+  },
+];
