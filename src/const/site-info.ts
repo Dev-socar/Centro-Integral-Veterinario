de98dd4fb1ebf7590c238f1ca2bccbd7b57c51a8
Dev-socar <omar.fsc19@gmail.com>
2024-11-09 04:15:36 -0600

    deploy app

---
+++ src/const/site-info.ts
@@ -0,0 +1,5 @@
+export const siteInfo = {
+  imagen: "logotipo.png",
+  description:
+    "En Centro Integral Veterinario, ofrecemos servicios de atención médica para tus mascotas. Contamos con un equipo de veterinarios expertos y atención personalizada para cada paciente.",
+};
